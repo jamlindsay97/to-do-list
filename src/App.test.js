31940c565e3c8963ac1043f49dfe89ba8aa58e23
James Lindsay <jamlindsay97@gmail.com>
2022-06-15 16:15:28 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Header tests", () => {
+  //makes sure that the app always has this specific text as the heading
+  //can use "test" or "it"
+  it("should find the header text", () => {
+    //the file that's being tested
+    render(<App />);
+    //query methods being used
+    const headerElement = screen.getByText(/To-do List/i);
+    //assertions
+    expect(headerElement).toBeInTheDocument();
+  });
+
+  it("should have a h1 tag as the main header element", () => {
+    render(<App />);
+    const headingElementType = screen.getByText(/To-do List/i);
+    expect(headingElementType).toContainHTML("h1");
+  });
 });
+
